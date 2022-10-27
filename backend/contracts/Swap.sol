5e9fb249a8f4ca7d9e4861b9d3fa3e2f2aafaed7
AsthaGarg16 <asthagarg1611@gmail.com>
2022-10-27 13:17:50 +0800

    minor updates

--- backend/contracts/Swap.sol
+++ backend/contracts/Swap.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract Swap is Ownable {
+    //for ETH-address=0, symbol="ETH"
     struct Token {
         address contractAddress;
         string symbolName;
@@ -34,11 +35,14 @@ contract Swap is Ownable {
     mapping(uint8 => Token) tokenInfo;
     mapping(uint8 => IERC20) tokens;
     mapping(address => mapping(uint8 => uint256)) tokenBalanceForAddress;
-    mapping(uint8 => mapping(uint8 => Market)) ExchangeMarket;
+    mapping(uint8 => mapping(string => string)) buyToSell;
+    mapping(uint8 => Market) ExchangeMarket;
     uint8 tokenIndex;
 
     constructor() {
-        tokenIndex = 1;
+        tokenIndex = 0;
+        Token ethToken = Token({contractAddress: "0x0", symbolName: "ETH"});
+        tokenInfo.push(0, ethToken);
     }
 
     /* EVENTS */
@@ -154,16 +158,10 @@ contract Swap is Ownable {
     {
         require(hasToken(symbolName));
         require(getBalanceForToken(symbolName) + amount >= getBalanceForToken(symbolName));
-
         uint8 _tokenIndex = getTokenIndex(symbolName);
-
-        IERC20 token = IERC20(tokenInfo[_tokenIndex].contractAddress);
-        require(token.transferFrom(msg.sender, address(this), amount) == true);
-
         tokenBalanceForAddress[msg.sender][_tokenIndex] += amount;
-
+        require(tokens[_tokenIndex].transferFrom(msg.sender, address(this), amount) == true);
         emit LogDepositToken(symbolName, msg.sender, amount, block.timestamp);
-
         return getBalanceForToken(symbolName);
     }
 
@@ -175,16 +173,155 @@ contract Swap is Ownable {
         require(amount <= getBalanceForToken(symbolName));
 
         uint8 _tokenIndex = getTokenIndex(symbolName);
-
-        IERC20 token = IERC20(tokenInfo[_tokenIndex].contractAddress);
-
         tokenBalanceForAddress[msg.sender][_tokenIndex] -= amount;
-
-        require(token.transfer(msg.sender, amount) == true);
+        require(tokens[_tokenIndex].transfer(msg.sender, amount) == true);
 
         emit LogWithdrawToken(symbolName, msg.sender, amount, block.timestamp);
-
         return getBalanceForToken(symbolName);
+    }
+
+    //doing beyond this
+
+    function createBuyOrder(
+        string memory symbolName,
+        uint256 priceInWei,
+        uint256 amount,
+        address buyer
+    ) private {
+        require(hasToken(symbolName));
+
+        uint8 _tokenIndex = getTokenIndex(symbolName);
+
+        uint256 _buy_amount_balance = amount;
+        // fulfil buyOrder by checking against which sell orders can be fulfil
+        if (ExchangeMarket[_tokenIndex].sellOrderBook.ordersCount > 0) {
+            _buy_amount_balance = fulfilBuyOrder(symbolName, _buy_amount_balance, priceInWei);
+        }
+        // check if buyOrder is fully fulfiled
+        if (_buy_amount_balance > 0) {
+            // update buyOrderBook - ordersQueue
+            (
+                uint256[] memory indexes,
+                uint256[] memory prices,
+                uint256[] memory amounts
+            ) = getBuyOrderBook(symbolName);
+            uint256 _newOrderIndex = ++tokens[_tokenIndex].buyOrderBook.orderIndex;
+            uint256[] memory _newOrdersQueue = new uint256[](_newOrderIndex);
+
+            bool _isOrderAdded = false;
+            if (tokens[_tokenIndex].buyOrderBook.ordersCount == 0) {
+                _newOrdersQueue[0] = _newOrderIndex;
+                _isOrderAdded = true;
+            } else {
+                uint256 _newOrdersQueueIndex = 0;
+                for (
+                    uint256 _counter = 0;
+                    _counter < tokens[_tokenIndex].buyOrderBook.ordersCount;
+                    _counter++
+                ) {
+                    if (!_isOrderAdded && priceInWei > prices[_counter]) {
+                        _newOrdersQueue[_newOrdersQueueIndex++] = _newOrderIndex;
+                        _isOrderAdded = true;
+                    }
+                    _newOrdersQueue[_newOrdersQueueIndex++] = tokens[_tokenIndex]
+                        .buyOrderBook
+                        .ordersQueue[_counter];
+                }
+                // for the case of the price being lower than the lowest price of the orderbook
+                if (!_isOrderAdded) {
+                    _newOrdersQueue[_newOrdersQueueIndex] = _newOrderIndex;
+                }
+            }
+
+            // replace existing orders queue is it's not empty
+            tokens[_tokenIndex].buyOrderBook.ordersQueue = _newOrdersQueue;
+
+            // Add new order to OrderBook
+            tokens[_tokenIndex].buyOrderBook.ordersCount++;
+            tokens[_tokenIndex].buyOrderBook.orders[_newOrderIndex] = Order({
+                price: priceInWei,
+                amount: _buy_amount_balance,
+                who: msg.sender
+            });
+
+            // fire event
+            emit LogCreateBuyOrder(
+                symbolName,
+                priceInWei,
+                _buy_amount_balance,
+                buyer,
+                block.timestamp
+            );
+        }
+    }
+
+    function fulfilBuyOrder(
+        string memory symbolName,
+        uint256 _buy_amount_balance,
+        uint256 priceInWei
+    ) private returns (uint256) {
+        uint8 _tokenIndex = getTokenIndex(symbolName);
+        uint256 _currSellOrdersCount = tokens[_tokenIndex].sellOrderBook.ordersCount;
+
+        uint256 _countSellOrderFulfiled = 0;
+
+        // update sellOrderBook - orders
+        for (uint256 i = 0; i < _currSellOrdersCount; i++) {
+            if (_buy_amount_balance == 0) break;
+
+            uint256 _orderIndex = tokens[_tokenIndex].sellOrderBook.ordersQueue[i];
+            uint256 _orderPrice = tokens[_tokenIndex].sellOrderBook.orders[_orderIndex].price;
+            uint256 _orderAmount = tokens[_tokenIndex].sellOrderBook.orders[_orderIndex].amount;
+            address _orderOwner = tokens[_tokenIndex].sellOrderBook.orders[_orderIndex].who;
+
+            if (priceInWei < _orderPrice) break;
+
+            if (_buy_amount_balance >= _orderAmount) {
+                _buy_amount_balance -= _orderAmount;
+
+                tokens[_tokenIndex].sellOrderBook.orders[_orderIndex].amount = 0;
+                _countSellOrderFulfiled++;
+                emit LogFulfilSellOrder(
+                    symbolName,
+                    _orderIndex,
+                    priceInWei,
+                    _orderAmount,
+                    block.timestamp
+                );
+
+                etherBalanceForAddress[_orderOwner] += priceInWei * _orderAmount;
+                tokenBalanceForAddress[msg.sender][_tokenIndex] += _orderAmount;
+            } else {
+                tokens[_tokenIndex].sellOrderBook.orders[_orderIndex].amount -= _buy_amount_balance;
+                emit LogFulfilSellOrder(
+                    symbolName,
+                    _orderIndex,
+                    priceInWei,
+                    _buy_amount_balance,
+                    block.timestamp
+                );
+
+                etherBalanceForAddress[_orderOwner] += priceInWei * _buy_amount_balance;
+                tokenBalanceForAddress[msg.sender][_tokenIndex] += _buy_amount_balance;
+
+                _buy_amount_balance = 0;
+            }
+        }
+
+        // update sellOrderBook - ordersBook and ordersCount
+        uint256 _newSellOrdersCount = _currSellOrdersCount - _countSellOrderFulfiled;
+
+        uint256[] memory _newSellOrdersQueue = new uint256[](_newSellOrdersCount);
+        for (uint256 i = 0; i < _newSellOrdersCount; i++) {
+            _newSellOrdersQueue[i] = tokens[_tokenIndex].sellOrderBook.ordersQueue[
+                i + _countSellOrderFulfiled
+            ];
+        }
+
+        tokens[_tokenIndex].sellOrderBook.ordersCount = _newSellOrdersCount;
+        tokens[_tokenIndex].sellOrderBook.ordersQueue = _newSellOrdersQueue;
+
+        return _buy_amount_balance;
     }
 
     //Helper functions
