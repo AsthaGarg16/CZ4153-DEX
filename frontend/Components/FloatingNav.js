fd67144adc3bbbd52e7ca7626a0952b323c746fa
AsthaGarg16 <asthagarg1611@gmail.com>
2022-11-01 19:11:45 +0800

    added markets integration

--- frontend/Components/FloatingNav.js
+++ frontend/Components/FloatingNav.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import swapAbi from "../constants/Swap.json"
-import { ethers } from "ethers"
-import { useWeb3Contract, useMoralis } from "react-moralis"
+import swapAbi from "../constants/Swap.json";
+// import networkMapping from "../constants/networkMapping.json";
+import { ethers } from "ethers";
+import { useWeb3Contract, useMoralis } from "react-moralis";
 
-const FloatingNav = () => {
+const FloatingNav = (swapAddress) => {
+  const { isWeb3Enabled, account } = useMoralis();
   const [marketPrice, setMarketPrice] = useState(0);
+  const [listofMarkets, setMarketList] = useState(["A/B", "B/C", "C/A"]); //to update
 
-  const getInitialState = () => {
-    const value = "A/B";
-    return value;
-  };
-  const [value, setOption] = useState(getInitialState);
+  // const getInitialState = () => {
+  //   const value = "A/B";
+  //   return value;
+  // };
+  const [value, setOption] = useState("");
 
   const handleChange = (e) => {
     setOption(e.target.value);
@@ -22,31 +25,66 @@ const FloatingNav = () => {
     // console.log(marketPrice)
   };
 
+  const { runContractFunction: getAllMarkets } = useWeb3Contract({
+    abi: swapAbi,
+    contractAddress: swapAddress,
+    functionName: "getAllMarkets",
+    params: {},
+  });
+
+  async function updateUI() {
+    var markets = await getAllMarkets();
+    var marketList = [];
+    console.log("getAllMarkets are ", markets);
+    if (markets) {
+      //markets[0] - buy token
+      //markets[1] - sell token
+      markets[0].forEach((x, i) => {
+        console.log("market ", x, "-", i);
+        marketList.push(x + "/" + markets[1][i]);
+      });
+      setMarketList(marketList);
+      setOption(marketList[0]);
+    }
+  }
+
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      updateUI();
+    }
+  }, [isWeb3Enabled]);
+
   return (
     <div>
       <div className="gap"></div>
       <div className="floatingnav">
-      <div className="widthgap"></div>
-      <div className="widthgap"></div>
-      <h3>Market:</h3>
-      <div className="widthgap"></div>
-      <div className="dropdown">
-      <div>
-          <select value={value} onChange={handleChange} className="button-81">
-            <option value="A/B">A/B</option>
-            <option value="B/C">B/C</option>
-            <option value="C/A">C/A</option>
-          </select>
-          {/* <p>{`You selected ${value}`}</p> */}
-      </div>
-      </div>
-      <div className="widthgapnew"></div>
-      <h3>Last Market Price: </h3>
-      <h4>{marketPrice} ETH</h4>
-      {/* <button onClick={handleChangeMP}>blabla</button> */}
+        <div className="widthgap"></div>
+        <div className="widthgap"></div>
+        <h3>Market:</h3>
+        <div className="widthgap"></div>
+        <div className="dropdown">
+          <div>
+            <select value={value} onChange={handleChange} className="button-81">
+              {/* {listofMarkets.map((item, index) => {
+                console.log(listofMarkets, item, index);
+                <option key={index} value={item}>
+                  {item}
+                </option>;
+              })} */}
+              <option value={listofMarkets[0]}>{listofMarkets[0]}</option>
+              <option value={listofMarkets[1]}>{listofMarkets[1]}</option>
+              <option value={listofMarkets[2]}>{listofMarkets[2]}</option>
+            </select>
+            {/* <p>{`You selected ${value}`}</p> */}
+          </div>
+        </div>
+        <div className="widthgapnew"></div>
+        <h3>Market Price: </h3>
+        <h4>{marketPrice}</h4>
+        {/* <button onClick={handleChangeMP}>blabla</button> */}
       </div>
     </div>
-  )
+  );
 };
 
 export default FloatingNav;
--- frontend/Components/OrderBook.js
+++ frontend/Components/OrderBook.js
@@ -1,53 +1,62 @@
-import * as React from 'react';
+import * as React from "react";
 import { useEffect, useState } from "react";
-import Box from '@mui/material/Box';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import { FixedSizeList } from 'react-window';
+import Box from "@mui/material/Box";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { FixedSizeList } from "react-window";
 
 function renderRow(props) {
   const { index, style } = props;
-  const [priceList, setPriceList] = useState([2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
-  const [qtyList, setQtyList] = useState([3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+  const [priceList, setPriceList] = useState([
+    2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
+  const [qtyList, setQtyList] = useState([
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
 
   return (
     <div>
       <ListItem style={style} key={index} component="div" disablePadding>
         <ListItemButton>
-          <ListItemText className='centertext' primary={`${priceList[index]}`} />
-          <ListItemText className='centertext' primary={`${qtyList[index]}`} />
-          <ListItemText className='centertext' primary={`${priceList[index]*qtyList[index]}`} />
+          <ListItemText
+            className="centertext"
+            primary={`${priceList[index]}`}
+          />
+          <ListItemText className="centertext" primary={`${qtyList[index]}`} />
+          <ListItemText
+            className="centertext"
+            primary={`${priceList[index] * qtyList[index]}`}
+          />
         </ListItemButton>
       </ListItem>
     </div>
-    
   );
 }
 
 export default function OrderBook() {
-  const [priceList, setPriceList] = useState([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+  const [priceList, setPriceList] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
   return (
     <div>
-    <div className='row1'>
-      <h4 className='customh4'>Price</h4>
-      <h4 className='customh4'>Qty</h4>
-      <h4 className='customh4'>Total</h4>
-    </div>
-    <div className="heightgapnew"></div>
-    <Box
-      sx={{ bgcolor: 'background.paper', borderRadius: '10px'}}
-    >
-      <FixedSizeList
-        height={240}
-        width={280}
-        itemSize={46}
-        itemCount={priceList.length}
-        overscanCount={5}
-      >
-        {renderRow}
-      </FixedSizeList>
-    </Box>
+      <div className="row1">
+        <h4 className="customh4">Price</h4>
+        <h4 className="customh4">Qty</h4>
+        <h4 className="customh4">Total</h4>
+      </div>
+      <div className="heightgapnew"></div>
+      <Box sx={{ bgcolor: "background.paper", borderRadius: "10px" }}>
+        <FixedSizeList
+          height={240}
+          width={280}
+          itemSize={46}
+          itemCount={priceList.length}
+          overscanCount={5}
+        >
+          {renderRow}
+        </FixedSizeList>
+      </Box>
     </div>
   );
 }
